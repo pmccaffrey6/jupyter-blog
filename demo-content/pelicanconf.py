cad1ea1c8376a5d1af80c23fb4057d7440bfb09d
Peter McCaffrey <pmccaffrey@tests-MacBook-Pro-2.local>
2017-07-12 01:14:03 -0400

    demo content

---
+++ demo-content/pelicanconf.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHORS = ['peter','jonathan']
+SITENAME = 'data-science-blog'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+DEFAULT_DATE = 'fs'
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# Main menu items
+HEADER_MENUITEMS = [('HOME', '/')]
+DISPLAY_PAGES_ON_MENU = False
+
+# Theme
+THEME = "themes/martin-pelican"
+
+# Ipynb plugin
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ['ipynb.markup']
